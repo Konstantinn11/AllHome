8c73d380cb99a58e2e5e28ef9ca81ae462e359c6
Konstantin <konstantin_mishukov@mail.ru>
2024-05-22 22:11:04 +0300

    That's all netochno!

--- products/admin.py
+++ products/admin.py
@@ -50,6 +50,19 @@ admin.site.register(Position_Price)
 class ZayavkaAdmin(admin.ModelAdmin):
     list_display = ('number', 'date_document', 'customer')
     list_filter = ('date_document', 'employer')
+    change_form_template = "admin/change_form.html"  # Here
+
+    actions = ['_generate_a_dogovor']
+
+    def _generate_a_dogovor(self, request, queryset):
+        pass
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+
+        extra_context['generate_a_dogovor'] = True  # Here
+
+        return super().changeform_view(request, object_id, form_url, extra_context)
 admin.site.register(Zayavka, ZayavkaAdmin)
 admin.site.register(UslugaCategory)
 admin.site.register(Usluga)
